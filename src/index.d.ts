6706e93172dcac63b0ad9c2aa1c0b0565366d087
jinpeng.lin <jinpeng.lin@anker-in.com>
2024-09-28 14:42:01 +0800

    feat:complete render function

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,20 @@
 type elementType = keyof HTMLElementTagNameMap
 type childrenType = (virtualDom | string)[]
+type props = {
+  [key in Exclude<keyof HTMLElement, "children">]?: HTMLElement[key]
+}
 
 interface virtualDom {
   type: elementType
-  props: Object | {}
+  props: {
+    children: (virtualDom | textDom)[]
+  } & props
 }
 
 interface textDom {
   type: "TEXT_ELEMENT"
-  props: Object
+  props: {
+    nodeValue: string
+    children: []
+  }
 }
--- src/react-dom.ts
+++ src/react-dom.ts
@@ -1,17 +1,36 @@
-export function render(element: virtualDom, container: HTMLElement) {}
+export function render(element: virtualDom | textDom, container: HTMLElement) {
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type)
+
+  const isProerty = (key: string) => key !== "children"
+
+  Object.keys(element.props)
+    .filter(isProerty)
+    // @ts-ignore
+    .forEach(name => (dom[name] = element.props[name]))
+
+  element.props.children.forEach(child => {
+    // textNode 的 children 为空，进不来这个循环 直接断言
+    render(child, dom as HTMLElement)
+  })
+
+  container.appendChild(dom)
+}
 
 export function createElement(
   type: elementType,
-  props?: Object | null,
+  props?: props | null,
   ...children: childrenType
 ): virtualDom {
   return {
     type,
     props: {
       ...props,
-      children: children.map(child => {
+      children: children.map(child =>
         typeof child === "object" ? child : createTextElement(child)
-      }),
+      ),
     },
   }
 }
